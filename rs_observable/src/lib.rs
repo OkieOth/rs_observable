37c84582a13ead0bed84e8fce9aec825c79b5a90
Eiko Thomas <eiko.thomas@gmail.com>
2024-01-15 23:10:28 +0100

    introduce features

--- rs_observable/src/lib.rs
+++ rs_observable/src/lib.rs
@@ -1,8 +1,12 @@
 mod observable;
 mod observed_value;
-//mod aobservable;
 mod chobservable;
 
+#[cfg(feature = "single")]
 pub use observable::{Observer, Observable};
+
+#[cfg(feature = "single")]
 pub use observed_value::ObservedValue;
+
+#[cfg(feature = "tokio")]
 pub use chobservable::{ChObservable, ChObservedValue};
